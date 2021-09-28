6fd7f7dfc127701e7fbf63ab0ca18199c3f22ec6
faramo_zayw <40602472+faramozzayw@users.noreply.github.com>
2021-09-28 21:06:40 +0300

    [Added] translate `into` section to ukrainian

---
+++ posts/uk/intro.md
@@ -0,0 +1,52 @@
+---
+title: 'Починаючи'
+description: 'Заснований на Promise HTTP клієнт для браузеру та Nodejs'
+next_title: 'Мінімальний приклад'
+next_link: '/uk/docs/example'
+---
+
+# Що таке Axios?
+Axios-це клієнт HTTP на основі *[Promise](https://javascript.info/promise-basics)* для [`node.js`](https://nodejs.org) та браузера. Він *[ізоморфний](https://www.lullabot.com/articles/what-is-an-isomorphic-application)* (= він може працювати у браузері та nodejs з тією ж базою кодів). На стороні сервера він використовує рідний `http`-модуль node.js, тоді як на клієнті (браузер) він використовує `XMLHttpRequests`.
+
+# Переваги
+
+- Робить [XMLHttpRequests](https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest) запити з браузеру
+- Робить [http](http://nodejs.org/api/http.html) запити з node.js
+- Supports the [Promise](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Promise) API
+- Перехопити запит і відповідь
+- Перетворення даних запиту та відповіді
+- Скасувати запити
+- Автоматичне перетворення даних JSON
+- Підтримка на стороні клієнта для захисту від [XSRF](http://en.wikipedia.org/wiki/Cross-site_request_forgery)
+
+# Встановлення
+
+Використовуючи npm:
+
+```bash
+$ npm install axios
+```
+
+Використовуючи bower:
+
+```bash
+$ bower install axios
+```
+
+Використовуючи yarn:
+
+```bash
+$ yarn add axios
+```
+
+Використовуючи jsDelivr CDN:
+
+```html
+<script src="https://cdn.jsdelivr.net/npm/axios/dist/axios.min.js"></script>
+```
+
+Використовуючи unpkg CDN:
+
+```html
+<script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+```
---
+++ uk.lang.js
@@ -0,0 +1,131 @@
+/**
+ * Configuration for the english (original) translation
+ */
+
+module.exports = {
+  // Language display name. MUST BE THE SAME AS IN [inert.config.js].custom.langs
+  display: "Українська",
+  prefix: "/uk/",
+  // `p` stands for `paragraph`. This will contain translations of full text blocks
+  p: {
+    headline: "Promise based HTTP client for the browser and node.js",
+    subhead: `Axios is a simple promise based HTTP client for the browser and node.js.
+              Axios provides a simple to use library in a small package with a very
+              extensible interface.`,
+  },
+  // `t` stands fot `translation`. This will contain translations of single words or phrases
+  t: {
+    "Get Started": "Починаючи",
+    "View on GitHub": "Переглянути на GitHub",
+    "Languages": "Мови",
+    "Open Source": "Open Source",
+    "Contribute": "Внесіть свій внесок",
+    "Source on GitHub": "Джерело на GitHub",
+    "Fork on GitHub": "Форкнути на GitHub",
+    "Fork the Website": "Форкнути сайт",
+    "Create an Issue": "Створіть Issue",
+  },
+  sidebar: [
+    {
+      type: "heading",
+      text: "Починаючи",
+    },
+    {
+      type: "link",
+      href: "/docs/intro",
+      text: "Вступ",
+    },
+    {
+      type: "link",
+      href: "/docs/example",
+      text: "Приклади",
+    },
+    {
+      type: "link",
+      href: "/docs/post_example",
+      text: "POST Запити",
+    },
+    {
+      type: "heading",
+      text: "Axios API",
+    },
+    {
+      type: "link",
+      href: "/docs/api_intro",
+      text: "Axios API",
+    },
+    {
+      type: "link",
+      href: "/docs/instance",
+      text: "Екземпляр Axios",
+    },
+    {
+      type: "link",
+      href: "/docs/req_config",
+      text: "Конфігурація запиту",
+    },
+    {
+      type: "link",
+      href: "/docs/res_schema",
+      text: "Схема відповіді",
+    },
+    {
+      type: "link",
+      href: "/docs/config_defaults",
+      text: "конфігурація за замовчуванням",
+    },
+    {
+      type: "link",
+      href: "/docs/interceptors",
+      text: "Перехоплювачі",
+    },
+    {
+      type: "link",
+      href: "/docs/handling_errors",
+      text: "Обробка помилок",
+    },
+    {
+      type: "link",
+      href: "/docs/cancellation",
+      text: "Скасування запиту",
+    },
+    {
+      type: "link",
+      href: "/docs/urlencoded",
+      text: "URL-кодування тіла",
+    },
+    {
+      type: "heading",
+      text: "Інше",
+    },
+    {
+      type: "link",
+      href: "/docs/notes",
+      text: "Замітки",
+    },
+    {
+      type: "heading",
+      text: "Контрибьютори",
+    },
+    {
+      type: "link",
+      href: "https://github.com/axios/axios/blob/master/CODE_OF_CONDUCT.md",
+      text: "Code of Conduct",
+    },
+    {
+      type: "link",
+      href: "https://github.com/axios/axios/blob/master/COLLABORATOR_GUIDE.md",
+      text: "Посібник для співавторів",
+    },
+    {
+      type: "link",
+      href: "https://github.com/axios/axios/blob/master/CONTRIBUTING.md",
+      text: "Внесок у Axios"
+    },
+    {
+      type: "link",
+      href: "/docs/translating",
+      text: "Переклад цієї документації"
+    }
+  ],
+};
