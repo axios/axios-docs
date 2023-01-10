5fcc9047c33067c67c918d42ab1f37159e3239c3
toto6038 <50100922+toto6038@users.noreply.github.com>
2023-01-10 18:53:39 +0800

    [Updated] config for Traditional Chinese

--- zhTW.lang.js
+++ zhTW.lang.js
@@ -1,60 +1,58 @@
 /**
- * Configuration for the english (original) translation
+ * Configuration for Traditional Chinese translation
  */
 
 module.exports = {
   // Language display name. MUST BE THE SAME AS IN [inert.config.js].custom.langs
-  display: "English",
-  prefix: "/",
+  display: "正體中文",
+  prefix: "/zhTW/",
   dir: "ltr",
-  lang: "en",
+  lang: "zhTW",
   // `p` stands for `paragraph`. This will contain translations of full text blocks
   p: {
-    headline: "Promise based HTTP client for the browser and node.js",
-    subhead: `Axios is a simple promise based HTTP client for the browser and node.js.
-              Axios provides a simple to use library in a small package with a very
-              extensible interface.`,
+    headline: "Axios 是一款簡易的 HTTP 客戶端函式庫，可在瀏覽器及 Node.js 環境中使用。",
+    subhead: `Axios 尺寸小巧，且提供了擴充彈性強大的介面。`,
   },
   // `t` stands fot `translation`. This will contain translations of single words or phrases
   t: {
-    "Get Started": undefined,
-    "View on GitHub": undefined,
-    "Languages": undefined,
-    "Open Source": undefined,
-    "Contribute": undefined,
-    "Source on GitHub": undefined,
-    "Issues": undefined,
-    "Pull Requests": undefined,
-    "Code of Conduct": undefined,
-    "Fork on GitHub": undefined,
-    "Fork the Website": undefined,
-    "Create an Issue": undefined,
-    "Next": undefined,
-    "Previous": undefined,
-    "Website Copy Right Footer": undefined,
-    "View On Github": undefined,
-    "Axios Project Copy Right Footer": undefined,
-    "License Label Footer": undefined
+    "Get Started": "開始使用",
+    "View on GitHub": "GitHub",
+    "Languages": "語言",
+    "Open Source": "開放原始碼",
+    "Contribute": "貢獻協作",
+    "Source on GitHub": "GitHub 儲存庫",
+    "Issues": "Issue",
+    "Pull Requests": "Pull Request",
+    "Code of Conduct": "行為準則",
+    "Fork on GitHub": "在 GitHub 上 fork 本專案",
+    "Fork the Website": "在 GitHub 上 fork 本網站",
+    "Create an Issue": "反應問題",
+    "Next": "下一頁",
+    "Previous": "上一頁",
+    "Website Copy Right Footer": "本網站：© 2020 至今 John Jakob \"Jake\" Sarjeant，版權所有。已獲准許使用。",
+    "View On Github": "在 GitHub 上檢視網站原始碼",
+    "Axios Project Copy Right Footer": "Axios 專案：© 2014 至今 Matt Zabriskie，版權所有。",
+    "License Label Footer": "本專案遵循"
   },
   sidebar: [
     {
       type: "heading",
-      text: "Getting Started",
+      text: "開始使用",
     },
     {
       type: "link",
       href: "/docs/intro",
-      text: "Introduction",
+      text: "介紹",
     },
     {
       type: "link",
       href: "/docs/example",
-      text: "Example",
+      text: "使用示例",
     },
     {
       type: "link",
       href: "/docs/post_example",
-      text: "POST Requests",
+      text: "POST 請求",
     },
     {
       type: "heading",
@@ -68,37 +66,37 @@ module.exports = {
     {
       type: "link",
       href: "/docs/instance",
-      text: "The Axios Instance",
+      text: "Axios 實例",
     },
     {
       type: "link",
       href: "/docs/req_config",
-      text: "Request Config",
+      text: "請求選項",
     },
     {
       type: "link",
       href: "/docs/res_schema",
-      text: "Response Schema",
+      text: "回應結構",
     },
     {
       type: "link",
       href: "/docs/config_defaults",
-      text: "Config Defaults",
+      text: "選項預設值",
     },
     {
       type: "link",
       href: "/docs/interceptors",
-      text: "Interceptors",
+      text: "Interceptor",
     },
     {
       type: "link",
       href: "/docs/handling_errors",
-      text: "Handling Errors",
+      text: "錯誤處理",
     },
     {
       type: "link",
       href: "/docs/cancellation",
-      text: "Cancellation",
+      text: "中止請求",
     },
     {
       type: "link",
@@ -107,36 +105,36 @@ module.exports = {
     },
     {
       type: "heading",
-      text: "Other",
+      text: "其他",
     },
     {
       type: "link",
       href: "/docs/notes",
-      text: "Notes",
+      text: "注意事項",
     },
     {
       type: "heading",
-      text: "Contributors",
+      text: "致貢獻者",
     },
     {
       type: "link",
       href: "https://github.com/axios/axios/blob/master/CODE_OF_CONDUCT.md",
-      text: "Code of Conduct",
+      text: "行為準則",
     },
     {
       type: "link",
       href: "https://github.com/axios/axios/blob/master/COLLABORATOR_GUIDE.md",
-      text: "Collaborator Guide",
+      text: "協作指引",
     },
     {
       type: "link",
       href: "https://github.com/axios/axios/blob/master/CONTRIBUTING.md",
-      text: "Contributing to Axios"
+      text: "貢獻 Axios"
     },
     {
       type: "link",
       href: "/docs/translating",
-      text: "Translating these docs"
+      text: "翻譯本文件"
     }
   ],
 };
